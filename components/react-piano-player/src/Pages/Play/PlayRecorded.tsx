0e119d2b3ee9a01b49347f81bd65a80503664b5d
eylulcankesici <github-eylulcankesici@gmail.com>
2025-12-18 19:26:29 +0300

    build

--- components/react-piano-player/src/Pages/Play/PlayRecorded.tsx
+++ components/react-piano-player/src/Pages/Play/PlayRecorded.tsx
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from 'react';
+import React,{useEffect,useState, useMemo} from 'react';
 
 import DrawPiano from '../../Components/DrawPiano/DrawPiano';
 import PlayingManagement from '../../Components/PlayingManagement/PlayingManagement';
@@ -6,12 +6,20 @@ import MidiPlayer from '../../Helpers/MidiPlayer';
 import { DefaultOptions } from '../../Utils/Default';
 import { Options as OptionsType } from '../../Utils/TypesForOptions';
 import { noteEvent } from "../../Utils/TypesForMidi";
+import soundManager from '../../Helpers/soundManager';
 
 export default function PlayRecorded() {
 
     const [options,setOptions] = useState<OptionsType>(DefaultOptions);
     const [Player,setPlayer] = useState<MidiPlayer>();
     const [Events,setEvents] = useState<Array<noteEvent>>();
+
+    const sound = useMemo(() => {
+        if (typeof window !== 'undefined') {
+            return new soundManager();
+        }
+        return undefined;
+    }, []);
 
     useEffect(() => {
         const options = JSON.parse(localStorage.getItem('options')!);
@@ -26,7 +34,7 @@ export default function PlayRecorded() {
 
     return (
         <div style={{overflow:'hidden'}}>
-            {Player &&<DrawPiano drawSpeed={options.playSpeed} Player={Player} Data={Events} Speed={options.speed} options={options}/>}
+            {Player &&<DrawPiano drawSpeed={options.playSpeed} Player={Player} Data={Events} Speed={options.speed} options={options} sound={sound}/>}
             {Player && <PlayingManagement Player={Player} onStart={()=>{}} />}
         </div>
     )
