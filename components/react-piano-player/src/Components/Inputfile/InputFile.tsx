ad5a69c850431345e037f8a7ea187664caba8c7f
eylulcankesici <github-eylulcankesici@gmail.com>
2025-12-18 19:02:12 +0300

    build

--- components/react-piano-player/src/Components/Inputfile/InputFile.tsx
+++ components/react-piano-player/src/Components/Inputfile/InputFile.tsx
@@ -61,7 +61,7 @@ export default function InputFile({FileRef,onFileUpload,options,onConfClick,isCo
                 <canvas className='backgroundCanvas' height={window.innerHeight + 300} width={window.innerWidth > 920 ? window.innerWidth / 2 : window.innerWidth} ref={Canvas}/>
             </div>
             <div className='FileInput_Data'>
-                <img src={MidiImage} alt='midi_icon' className='Midi_Icon'  />
+                <img src={MidiImage.src} alt='midi_icon' className='Midi_Icon'  />
                 <h2 className='Input_Text'>Drag Your MIDI file here to start visualizing!</h2>
                 <h3 className='Input_Text'>Or click here to choose file!</h3>
                 <button className='Input_Configure_Bt' onClick={onConfigureClick}>Configure</button>
