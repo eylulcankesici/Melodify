0fc57e7dbd6cacbd6dccd00852849163db9204dc
eylulcankesici <github-eylulcankesici@gmail.com>
2025-12-18 19:18:37 +0300

    build

--- components/react-piano-player/src/react-app-env.d.ts
+++ components/react-piano-player/src/react-app-env.d.ts
@@ -4,3 +4,7 @@ declare module '*.MID';
 declare module '*.ogg';
 declare module '*.png';
 declare module '*.jpeg';
+declare module 'hex2dec' {
+  export function decToHex(dec: number): string;
+  export function hexToDec(hex: string): number;
+}
